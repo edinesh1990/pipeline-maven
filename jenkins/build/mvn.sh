d2b50ac9f0764fdde4caf3c7bf0c1ca0fa09ba49
Dinesh Mudaliar <edinesh.90@gmail.com>
2020-03-04 19:24:15 +0000

    correct workspace path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "*****************************"
 echo "******Building jar***********"
 echo "*****************************"
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/centos/jenkins-data/pipeline/
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
